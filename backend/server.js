c5cf00495135ed18f908e30a8d966d0a3b34a9b1
ssang <sungsofiya@gmail.con>
2021-05-19 21:21:14 +0600

    first commit

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,10 @@ connection.once('open', () => {
     console.log("Mongodb database connection established successfully !!");
 })
 
-const exercisesRouter = require('./routes/exercises');
+
 const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRouter);
+
 app.use('/users', usersRouter);
 
 
